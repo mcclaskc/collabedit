d1bd151f467315cc9a67787a5f0d9e406b0bbc7b
Chris McClaskey <christopher.m.mcclaskey@gmail.com>
2013-04-11 00:23:28 -0600

    created Clasky class

---
+++ ruby/clasky.rb
@@ -0,0 +1,19 @@
+class Clasky
+    
+    @@awesome = true
+    @@instances = 0;
+    
+    def initialize
+        raise "There can only be one claskylander!" if @@instances == 1
+        @@instances += 1
+    end
+    
+    def self.is_awesome?
+        @@awesome
+    end
+    
+    def is_awesome?
+        @@awesome
+    end
+    
+end
